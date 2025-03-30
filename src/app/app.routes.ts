a9a559fe5a477fb18a6f62622a6702e869e36115
AngelicaGomez08 <43476564+AngelicaGomez08@users.noreply.github.com>
2025-03-29 20:43:44 -0500

    Subiendo backend, frontend y scripts_bd

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './users/login/login.component';
-import { UserListComponent } from './users/user-list/user-list.component';
-
-export const routes: Routes = [ 
-    { path: 'login', component: LoginComponent },
-    { path: 'users', component: UserListComponent },
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
-];
--- src/app/users/user.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    id?: number;
-    firstName: string;
-    lastName: string;
-    identification: string;
-    email: string;
-    lastAccessDate?: string;
-  }
