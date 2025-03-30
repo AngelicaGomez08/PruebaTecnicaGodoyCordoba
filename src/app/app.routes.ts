da595af9e17f78dc2f9f8404c82308e363d57217
angelica <angelica.gomez.474@hotmail.com>
2025-03-29 19:54:33 -0500

    Añadir y modificar archivos del frontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './users/login/login.component';
+import { UserListComponent } from './users/user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+    { path: 'login', component: LoginComponent },
+    { path: 'users', component: UserListComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
